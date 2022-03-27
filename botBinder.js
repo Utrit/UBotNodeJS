7958369a93f1c02f5b61c076cd9e42ab93b20e02
Utrit <vadyasrus@gmail.com>
2022-03-28 04:32:05 +0500

    small bugfixes and ban tags

--- botBinder.js
+++ botBinder.js
@@ -13,40 +13,40 @@ const interactionEvent = new EventEmitter;
 var commandModules = filesystem.readdirSync(__dirname+config.modulePath);
 var commandPrototypes = [];
 var logchannel;
-console.log("Regestration commands:");
+console.log("\x1b[32mRegestration commands:");
 for (let index = 0; index < commandModules.length; index++) {
     commandPrototypes[index] = require(__dirname+config.modulePath+commandModules[index]);
-    console.log(`${index} - ${commandPrototypes[index].command} registred`);
+    console.log(`\x1b[33m${index} - ${commandPrototypes[index].command} registred`);
 }
-console.log("Regestration complete");
+console.log("\x1b[32mRegestration complete");
 
 client.login(config.token);
 
 client.once("ready", () => {
-    console.log("Discord client ready");
-    console.log("Regestration events");
+    console.log("\x1b[32mDiscord client ready");
+    console.log("\x1b[32mRegestration events");
     for (let index = 0; index < commandPrototypes.length; index++) {
         commandEvent.on(commandPrototypes[index].command,commandPrototypes[index].doCommand)
-        console.log(`${index} - ${commandPrototypes[index].command} registred`);
+        console.log(`\x1b[33m${index} - ${commandPrototypes[index].command} registred`);
         if(commandPrototypes[index].hasInteraction){
             interactionEvent.on(commandPrototypes[index].command,commandPrototypes[index].doInteraction)
-            console.log(`${index} - ${commandPrototypes[index].command} interaction registred`);
+            console.log(`\x1b[33m${index} - ${commandPrototypes[index].command} interaction registred`);
         }
     }
-    console.log("Regestration complete");
+    console.log("\x1b[32mRegestration complete");
     console.log("Start heartbeat");
     logchannel = client.channels.cache.get("805764161689616454");
     setInterval(() => {
         commandEvent.emit("goodday",logchannel)
     }, 5000);
-    console.log("Heartbeat started");
+    console.log("\x1b[32mHeartbeat started");
 });
 client.on("messageCreate", async message => {
-    messageArgs = message.content.split(' ');
+    messageArgs = message.content.toLowerCase().split(' ');
     if(!messageArgs[0].startsWith(config.prefix)) return;
-    let command = messageArgs[0].substring(1,messageArgs[0].length).toLowerCase();
+    let command = messageArgs[0].substring(1,messageArgs[0].length);
     if(alials[command]) command = alials[command];
-    console.log(`call ${command} - by ${message.author.username} id - ${message.author.id}`);
+    console.log(`\x1b[36mcall ${command} - by ${message.author.username} id - ${message.author.id}`);
     commandEvent.emit(command,message,messageArgs,commandPrototypes)
 })
 client.on('interactionCreate', interaction => {
