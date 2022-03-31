88d42de4d71342602a947d4b2cb6e26dcb94bdd8
Utrit <vadyasrus@gmail.com>
2022-03-31 08:05:06 +0500

    added pet command

---
+++ commands/pet.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js")
+const { MessageEmbed, MessageActionRow, MessageButton, MessageAttachment } = require('discord.js');
+const config = require("../config.json")
+const alials = require("../alials.json");
+const petPetGif = require('pet-pet-gif')
+const command = "pet"
+const syntaxes = "погладить @user"
+const help = "Создает гифку глажки по айди человека"
+const errcode = {
+    "delete": "Удалить сообщение может только тот кто его отправил",
+}
+
+const hasInteraction = true;
+async function doCommand(message, messageArgs, commandPrototypes) {
+    if (message.channel.type == "DM" || messageArgs[1] == undefined) return;
+    member = message.guild.members.cache.get(messageArgs[1].replace(/[^0-9]/ig, ''));
+    if (member == undefined) {
+        message.reply("Данный аккаунт не найден")
+        return;
+    }
+    user = member.user
+    let animatedGif = await petPetGif(
+        member.displayAvatarURL({ format: "png", size: 256 }),
+        {
+            resolution: 200,
+        });
+    const file = new MessageAttachment(animatedGif, "pet.gif");
+    let customInfodel = { "call": command, "id": message.author.id, "do": "delete" };
+    let rowButton = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setLabel('Удалить')
+                .setCustomId(JSON.stringify(customInfodel))
+                .setStyle('PRIMARY')
+        )
+    let embed = new MessageEmbed()
+        .setAuthor({ name: `${message.author.username} Жмякает ${user.username}`, iconURL: message.author.avatarURL() })
+        .setImage("attachment://pet.gif")
+    message.channel.send({ embeds: [embed], components: [rowButton], files: [file] });
+}
+function doInteraction(interaction, buttoninfo) {
+    if (!(interaction.user.id == 198570626082930689 || interaction.user.id == buttoninfo.id)) {
+        interaction.reply({ ephemeral: true, content: errcode[buttoninfo.do] })
+        return;
+    }
+    if (buttoninfo.do == "delete") interaction.message.delete();
+}
+module.exports = { doCommand, command, hasInteraction, syntaxes, doInteraction, help }
